a4e6c41d9d324f75798dcf13abe380cb76853621
jianbo <1784749609@qq.com>
2024-08-11 20:33:51 +0800

    init

---
+++ src/main/java/com/titool/log/LogUtilApplication.java
@@ -0,0 +1,13 @@
+package com.titool.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogUtilApplication.class, args);
+    }
+
+}
